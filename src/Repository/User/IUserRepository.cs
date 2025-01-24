d6f9d89d3e31187c0301c4e4f730a4caa1e862be
UHDFox <108611791+UHDFox@users.noreply.github.com>
2025-01-24 21:32:43 +0300

    Change projects location

---
+++ src/Repository/User/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.Users;
+
+namespace Repository.User;
+
+public interface IUserRepository
+{
+    Task<IReadOnlyCollection<UserRecord>> GetAllAsync(int offset, int limit);
+
+    Task<UserRecord> GetByEmailAsync(string email);
+
+    Task<int> GetTotalAmountAsync();
+
+    Task<UserRecord?> GetByIdAsync(Guid id);
+
+    Task<Guid> AddAsync(UserRecord data);
+
+    void Update(UserRecord data);
+
+    Task<bool> DeleteAsync(Guid id);
+
+    Task<int> SaveChangesAsync();
+}
---
+++ src/Repository/User/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Domain;
+using Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.User;
+
+public sealed class UserRepository : IUserRepository
+{
+    protected FahrenheitAuthDbContext _context;
+
+    public UserRepository(FahrenheitAuthDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IReadOnlyCollection<UserRecord>> GetAllAsync(int offset, int limit)
+    {
+        return await _context.Set<UserRecord>().Skip(offset).Take(limit).ToListAsync();
+    }
+
+    public async Task<UserRecord> GetByEmailAsync(string email)
+    {
+        return await _context.Set<UserRecord>().FirstOrDefaultAsync(e => e.Email == email)
+               ?? throw new Exception("Email doesn't exist");
+    }
+
+    public async Task<int> GetTotalAmountAsync()
+    {
+        return await _context.Set<UserRecord>().CountAsync();
+    }
+
+    public async Task<UserRecord?> GetByIdAsync(Guid id)
+    {
+        return await _context.Set<UserRecord>().AsNoTracking().FirstOrDefaultAsync(record => record.Id == id);
+    }
+
+    public async Task<Guid> AddAsync(UserRecord data)
+    {
+        await _context.Set<UserRecord>().AddAsync(data);
+        await SaveChangesAsync();
+        return data.Id;
+    }
+
+    public void Update(UserRecord data)
+    {
+        _context.Set<UserRecord>().Update(data);
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        _context.Set<UserRecord>().Remove((await GetByIdAsync(id))!);
+        return await SaveChangesAsync() > 0;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
